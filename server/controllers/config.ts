131f489bc76e3acaede77be23c1895ba47e817a7
Mickaël Depardon <mickael.depardon@wizbii.com>
2024-04-02 15:00:11 +0200

    feat(server): add endpoint to get configured content types

---
+++ server/controllers/config.ts
@@ -0,0 +1,24 @@
+import { Strapi } from '@strapi/strapi';
+import Koa from 'koa';
+import { StrapiAlgoliaConfig } from '../../utils/config';
+
+export default ({ strapi }: { strapi: Strapi }) => ({
+  async contentTypes(
+    ctx: Koa.Context & {
+      request: {
+        body?: unknown;
+        rawBody: string;
+      };
+    }
+  ) {
+    const { contentTypes } = strapi.config.get(
+      'plugin.strapi-algolia'
+    ) as StrapiAlgoliaConfig;
+
+    if (!contentTypes) {
+      return;
+    }
+
+    ctx.body = { contentTypes };
+  },
+});
--- server/controllers/index.ts
+++ server/controllers/index.ts
@@ -1,5 +1,7 @@
+import config from './config';
 import indexAll from './index-all';
 
 export default {
   'strapi-algolia-index-articles': indexAll,
+  'strapi-algolia-config': config,
 };
