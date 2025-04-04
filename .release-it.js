6f6f5d6394adb7593ec4e370b9a6b17a66a1884b
Mickaël Depardon <mickael.depardon@wizbii.com>
2025-04-04 14:54:15 +0200

    chore: show all commit types in CHANGELOG.md

--- .release-it.js
+++ .release-it.js
@@ -11,30 +11,11 @@ module.exports = {
   },
   plugins: {
     '@release-it/conventional-changelog': {
-      writerOpts: {
-        commitPartial: commitTemplate,
-      },
+      writerOpts: { commitPartial: commitTemplate },
       infile: 'CHANGELOG.md',
-      preset: {
-        name: 'conventionalcommits',
-        types: [
-          { type: 'feat', section: 'Features' },
-          { type: 'fix', section: 'Bug Fixes' },
-          { type: 'chore', hidden: 'Chores' },
-          { type: 'docs', section: 'Docs' },
-          { type: 'style', section: 'Styles' },
-          { type: 'refactor', section: 'Refactor' },
-          { type: 'perf', hidden: 'true' },
-          { type: 'test', section: 'Tests' },
-          { type: 'build', hidden: 'true' },
-          { type: 'ci', section: 'CI' },
-          { type: 'revert', hidden: 'true' },
-        ],
-      },
+      preset: { name: 'conventionalcommits' },
       ignoreRecommendedBump: 'true',
     },
   },
-  npm: {
-    publish: false,
-  },
+  npm: { publish: false },
 };
